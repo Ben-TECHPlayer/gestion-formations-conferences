79dcb3ae8368d80cd3e207aae27ae1f98a4d8abf
Ben Ali Aboudou <158784500+Ben-TECHPlayer@users.noreply.github.com>
2026-07-03 22:19:12 +0200

    Je me projette un peu sur les formations en école de commerce

--- src/pages/formations/FormationEcoleDeCommerce.js
+++ src/pages/formations/FormationEcoleDeCommerce.js
@@ -14,17 +14,48 @@ function FormationEcoleDeCommerce(){
                     Résumé
                 </NavLink>
                 <NavLink to="/formations/formation-college" className={({ isActive }) => isActive ? "active" : ""}>
+                    Programme
+                </NavLink>
+                <NavLink to="/formations/formation-college" className={({ isActive }) => isActive ? "active" : ""}>
                     Pourquoi ce cursus ?
                 </NavLink>
                 <NavLink to="/formations/formation-college" className={({ isActive }) => isActive ? "active" : ""}>
                     Admission
                 </NavLink>
+                <NavLink to="/formations/formation-college" className={({ isActive }) => isActive ? "active" : ""}>
+                    Débouchés
+                </NavLink>
             </div>
 
-            <h1>
-                Les avantages pour les collégiens
-            </h1>
+            <h1>Résumé</h1>
+            <p>
+                Cette formation a pour but : former les futurs génies du commerce, pour cela on a plusieurs débouchés qu'on verra plus tard.
+                C'est pourquoi nous vous proposons l'alternance en école de commerce, en effet cela va vous permet de vous créer des opportunités qui vous seront présentées, alors saisissez-les, et surtout ne les laissez pas passer!
+            </p>
+            <h1>Programme</h1>
             <p></p>
+            <h1>Les avantages pour les futurs étudiants en école de commerce</h1>
+            <ul>
+                <li>Formation gratuite</li>
+                <li>Diplôme reconnu par l'Etat</li>
+                <li>Rémunération généreuse</li>
+                <li>Vie étudiante au sommet</li>
+                <li>Expérience professionnelle à l'international</li>
+            </ul>
+            <h1>Admission</h1>
+            <p>L'entrée se fait en 3ème année d'école de commerce par défaut, et elle est postulable de plusieurs manières :</p>
+            <ul>
+                <li>Par voie classique en 5 ans, après le Bac STMG</li>
+                <li>Après le BAC +3, notamment le BUT TC</li>
+                <li>Via les admissions parallèles avec une deuxième année validée en BUT Techniques de Commercialisation</li>
+            </ul>
+            <h1>Débouchés</h1>
+            <ul>
+                <li>Commercial</li>
+                <li>Technico-commercial</li>
+                <li>Ingénieur commercial</li>
+                <li>Business Intelligence</li>
+            </ul>
         </main>
     );
 }
