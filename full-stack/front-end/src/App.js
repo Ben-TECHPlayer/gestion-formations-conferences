57b081225f7338b3380acb1afa5084d57a2473fc
Ben Ali Aboudou <158784500+Ben-TECHPlayer@users.noreply.github.com>
2026-07-26 22:02:22 +0200

    Restauration du code pour prendre de bonnes pratiques sur nos branches Git par la suite

--- full-stack/front-end/src/App.js
+++ full-stack/front-end/src/App.js
@@ -2,55 +2,15 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
-// Importer nos composants réutilisables
-import Header from './components/Header';
-import Footer from './components/Footer';
-
-// Importer nos pages
-import Home from './pages/Home';
-import About from './pages/About';
-
-// Pages de cours
-import CoursCollege from './pages/cours/CoursCollege';
-
-// Paiement
-import Paiement from './pages/Paiement';
-
-// ---- Importer nos feuilles de style ---- //
-
-// Customiser nos composants
-import './styles/Footer.css';
-import './styles/Header.css';
-
-// Customiser nos pages
-import './styles/pages/Home.css';
-import './styles/pages/About.css';
-import './styles/pages/Cours.css';
-import './styles/pages/Paiement.css';
-
-import './styles/index.css';
-
 function App() {
     return (
         <div>
             {/* Ajouter notre routeur et mettre en marche pour naviguer */}
             <Router>
                 <div className="App">
-                    {/* Ajouter notre header qui est notre menu */}
-                    <Header />
-                    {/* Ajouter nos routes qui sont nos pages */}
-                    <Routes>
-                        <Route path="/" element={<Home />} /> 
-                        <Route path="/about" element={<About/>} />
-
-                        {/* Partie Clients */}
-                        <Route path="/cours/collegiens" element={<CoursCollege />} /> 
-                        <Route path="/paiement" element={<Paiement />} />
-                        {/* <Route path="/conferences" element={<Conferences />} /> */}
-                    </Routes>
-                  </div>
-                </Router>
-          <Footer />
+                    
+                </div>
+            </Router>
         </div>
     );
 }
