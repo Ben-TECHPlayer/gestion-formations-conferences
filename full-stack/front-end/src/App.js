bfd1e454f12e64b9ea9f43a8874c0fd73275d4ec
Ben Ali Aboudou <158784500+Ben-TECHPlayer@users.noreply.github.com>
2026-07-27 17:36:08 +0200

    Ajout de ma page d'accueil dans le main

--- full-stack/front-end/src/App.js
+++ full-stack/front-end/src/App.js
@@ -2,13 +2,24 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
+// Importer nos pages
+import Home from './pages/Home';
+
+// ---- Importer nos feuilles de style ---- //
+
+// Customiser nos pages
+import './styles/pages/Home.css';
+
 function App() {
     return (
         <div>
             {/* Ajouter notre routeur et mettre en marche pour naviguer */}
             <Router>
                 <div className="App">
-                    
+                    {/* Ajouter nos routes qui sont nos pages */}
+                    <Routes>
+                        <Route path="/" element={<Home />} /> 
+                    </Routes>
                 </div>
             </Router>
         </div>
