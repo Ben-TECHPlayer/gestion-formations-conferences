8d66cd935f3f5e63b01a9f4c518615e3f8d31014
Ben Ali Aboudou <158784500+Ben-TECHPlayer@users.noreply.github.com>
2026-07-27 17:47:59 +0200

    Sauvergarde partielle de notre branche principale

--- full-stack/front-end/src/App.js
+++ full-stack/front-end/src/App.js
@@ -2,13 +2,13 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
-<<<<<<< HEAD
 // Importer nos composants réutilisables
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 // Importer nos pages
 import Home from './pages/Home';
+import About from './pages/About';
 
 // ---- Importer nos feuilles de style ---- //
 
@@ -18,15 +18,7 @@ import './styles/Footer.css';
 
 // Customiser nos pages
 import './styles/pages/Home.css';
-=======
-// Importer nos pages
-import About from './pages/About';
-
-// ---- Importer nos feuilles de style ---- //
-
-// Customiser nos pages
 import './styles/pages/About.css';
->>>>>>> feature/presentation-entreprise
 
 import './styles/index.css';
 
@@ -36,22 +28,15 @@ function App() {
             {/* Ajouter notre routeur et mettre en marche pour naviguer */}
             <Router>
                 <div className="App">
-<<<<<<< HEAD
                     {/* Ajouter notre header qui est notre menu */}
                     <Header/>
                     {/* Ajouter nos routes qui sont nos pages */}
                     <Routes>
-                        <Route path="/" element={<Home />} /> 
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<About/>} /> 
                     </Routes>
                     {/* Ajouter notre footer */}
-                    <Footer/>
-                    
-=======
-                    {/* Ajouter nos routes qui sont nos pages */}
-                    <Routes>
-                        <Route path="/about" element={<About/>} />
-                    </Routes>                    
->>>>>>> feature/presentation-entreprise
+                    <Footer/>                 
                 </div>
             </Router>
         </div>
