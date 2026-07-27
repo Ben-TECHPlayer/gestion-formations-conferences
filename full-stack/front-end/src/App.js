5029df99af3b4f2a244e0be93c99566e964bada6
Ben Ali Aboudou <158784500+Ben-TECHPlayer@users.noreply.github.com>
2026-07-27 18:00:19 +0200

    Ajout de notre cours pour les collégiens

--- full-stack/front-end/src/App.js
+++ full-stack/front-end/src/App.js
@@ -2,13 +2,27 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
+// Pages de cours
+import CoursCollege from './pages/cours/CoursCollege';
+
+// ---- Importer nos feuilles de style ---- //
+
+// Customiser nos pages
+import './styles/pages/Cours.css';
+
+import './styles/index.css';
+
 function App() {
     return (
         <div>
             {/* Ajouter notre routeur et mettre en marche pour naviguer */}
             <Router>
                 <div className="App">
-                    
+                    {/* Ajouter nos routes qui sont nos pages */}
+                    <Routes>
+                        {/* Partie Clients */}
+                        <Route path="/cours/collegiens" element={<CoursCollege />} /> 
+                    </Routes>
                 </div>
             </Router>
         </div>
