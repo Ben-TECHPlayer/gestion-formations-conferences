a2860872ba31a7f93d4c85d913b56ac58c953ad9
Ben Ali Aboudou <158784500+Ben-TECHPlayer@users.noreply.github.com>
2026-07-27 17:12:04 +0200

    Ajout de mon footer dans le main

--- full-stack/front-end/src/App.js
+++ full-stack/front-end/src/App.js
@@ -2,6 +2,14 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
+// Importer nos composants réutilisables
+import Footer from './components/Footer';
+
+// ---- Importer nos feuilles de style ---- //
+
+// Customiser nos composants
+import './styles/Footer.css';
+
 function App() {
     return (
         <div>
