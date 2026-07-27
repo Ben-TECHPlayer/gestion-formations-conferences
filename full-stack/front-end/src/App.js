35404951f6787793f65fb1c09c899d3e2cacc05e
Ben Ali Aboudou <158784500+Ben-TECHPlayer@users.noreply.github.com>
2026-07-27 17:43:59 +0200

    Ajout de la présentation de l'entreprise dans la branche principale

--- full-stack/front-end/src/App.js
+++ full-stack/front-end/src/App.js
@@ -2,13 +2,26 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
+// Importer nos pages
+import About from './pages/About';
+
+// ---- Importer nos feuilles de style ---- //
+
+// Customiser nos pages
+import './styles/pages/About.css';
+
+import './styles/index.css';
+
 function App() {
     return (
         <div>
             {/* Ajouter notre routeur et mettre en marche pour naviguer */}
             <Router>
                 <div className="App">
-                    
+                    {/* Ajouter nos routes qui sont nos pages */}
+                    <Routes>
+                        <Route path="/about" element={<About/>} />
+                    </Routes>                    
                 </div>
             </Router>
         </div>
